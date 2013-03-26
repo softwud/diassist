93bc007323ccbbaa128ccf830d9f7cd58e6c1391
softwud@softwud.com <softwud@softwud.com>
2013-03-26 14:30:16 +1100

    Still learning development

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,8 @@ var app = {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
         app.report('deviceready');
-		this.showAlert('message', 'title');
+		//this.showAlert('message', 'title');
+//		console.log('ready');
     },
     report: function(id) {
         // Report the event in the console
@@ -36,12 +37,12 @@ var app = {
         // Toggle the state from "pending" to "complete" for the reported ID.
         // Accomplished by adding .hide to the pending element and removing
         // .hide from the complete element.
-		//document.querySelector('#' + id + ' .pending').className += ' hide';
-        //var completeElem = document.querySelector('#' + id + ' .complete');
-        //completeElem.className = completeElem.className.split('hide').join('');
+		document.querySelector('#' + id + ' .pending').className += ' hide';
+        var completeElem = document.querySelector('#' + id + ' .complete');
+        completeElem.className = completeElem.className.split('hide').join('');
     },
 
-	showAlert:
+/*	showAlert:
 		function (message, title)
 		{
 			if (navigator.notification)
@@ -52,5 +53,5 @@ var app = {
 			{
 				alert(title ? (title + ": " + message) : message);
 			}
-		}
+		}*/
 };
